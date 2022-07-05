b1a4143c868811ed86532a7f0916507c0122b10d
Kyle VanTassell <octicon@gmail.com>
2022-07-05 17:04:04 -0500

    bonus calc

--- client.js
+++ client.js
@@ -39,4 +39,68 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+//**
+// * 
+// * @param {*} employeeObj 
+// * @param {*} employees 
+// */
+
+function calculateBonus(cohortArray) {
+  // let empBonus = "";
+
+  for (employeeObj of cohortArray) {
+    console.log('Calculating bonus for', employeeObj.name);
+    // empBonus += "<li>" + newObject + "</li>";
+    let bonusPercent = 0;
+
+    if (employeeObj.rating <= 2) {
+      console.log('No bonus', employeeObj.name);
+      bonusPercent = 0;
+    } else if (employeeObj.reviewRating === 3) {
+      bonusPercent += 0.04;
+    } else if (employeeObj.reviewRating === 4) {
+      bonusPercent += 0.06;
+    } else if (employeeObj.reviewRating === 5) {
+      bonusPercent += 0.1;
+    };
+
+    if (employeeObj.employeeNumber.length === 4) {
+      bonusPercent += 0.05;
+    }
+
+    if (parseInt(employeeObj.annualSalary) > 65000) {
+      bonusPercent -= 0.01;
+    }
+
+    if (bonusPercent > .13) {
+      bonusPercent = .13;
+    } else if (bonusPercent <= 0) {
+      bonusPercent = 0;
+    }
+
+    let bonusAmount = parseInt(employeeObj.annualSalary) * bonusPercent;
+
+    let totalComp = Math.ceil(parseInt(employeeObj.annualSalary) + bonusAmount);
+
+    let newObject = {
+      name: employeeObj.name,
+      bonusPercent: bonusPercent,
+      bonusAmount: bonusAmount,
+      totalComp: totalComp,
+    }
+    console.log(newObject);
+
+  }
+  // bonusData.innerHTML = empBonus;
+};
+
+
+
+
+
+
+
+
+
